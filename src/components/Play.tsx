c7499708ed66a4022dc36965e41a45d90af19be9
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-12 20:36:03 -0300

    implementing onClick for each image

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -7,13 +7,23 @@ export default function Play() {
     <>
       <div className="flex justify-center mt-24">
         <div className="px-10">
-          <img src={rock} alt="logo" className="h-40 hover:cursor-pointer" />
+          <img
+            src={rock}
+            alt="logo"
+            className="h-40 hover:cursor-pointer"
+            onClick={() => console.log("pedra")}
+          />
           <p className=" text-center text-gray-800 dark:text-gray-100 text-lg font-black ">
             Pedra
           </p>
         </div>
         <div className="px-10">
-          <img src={paper} alt="logo" className="h-40 hover:cursor-pointer" />
+          <img
+            src={paper}
+            alt="logo"
+            className="h-40 hover:cursor-pointer"
+            onClick={() => console.log("papel")}
+          />
           <p className=" text-center text-gray-800 dark:text-gray-100 text-lg font-black ">
             Papel
           </p>
@@ -23,6 +33,7 @@ export default function Play() {
             src={scissors}
             alt="logo"
             className="h-40 hover:cursor-pointer"
+            onClick={() => console.log("tesoura")}
           />
           <p className=" text-center text-gray-800 dark:text-gray-100 text-lg font-black ">
             Tesoura
