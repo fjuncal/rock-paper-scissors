ece919e2aa4df75e369ff89f2b2e0f1615c3d7f9
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-12 20:32:24 -0300

    refactoring logo imagem and background color main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import logo from "./assets/teste.png";
-import scissors from "./assets/scissors.png";
-import paper from "./assets/paper.png";
-import rock from "./assets/rock.png";
+import logo from "./assets/logo.png";
+
+import Play from "./components/Play";
 
 function App() {
   return (
     <>
-      <div className="flex justify-center ">
-        <img src={logo} alt="logo" className="h-40" />
-      </div>
-      <div className="">
+      <div className="bg-red-400 h-screen">
+        <div className="flex justify-center py-4 ">
+          <img src={logo} alt="logo" className="h-40" />
+        </div>
         <div className="">
           <Header />
+        </div>
+        <p className=" text-center text-gray-800 dark:text-gray-100 text-3xl font-bold mt-8">
+          Escolha uma das opções:
+        </p>
+        <div className="flex justify-center">
+          <Play />
         </div>
         <div className="fixed left-0 bottom-0 w-full mb-8 text-center">
           <Footer />
---
+++ src/components/Play.tsx
@@ -0,0 +1,34 @@
+import scissors from "../assets/scissors.png";
+import paper from "../assets/paper.png";
+import rock from "../assets/rock.png";
+
+export default function Play() {
+  return (
+    <>
+      <div className="flex justify-center mt-24">
+        <div className="px-10">
+          <img src={rock} alt="logo" className="h-40 hover:cursor-pointer" />
+          <p className=" text-center text-gray-800 dark:text-gray-100 text-lg font-black ">
+            Pedra
+          </p>
+        </div>
+        <div className="px-10">
+          <img src={paper} alt="logo" className="h-40 hover:cursor-pointer" />
+          <p className=" text-center text-gray-800 dark:text-gray-100 text-lg font-black ">
+            Papel
+          </p>
+        </div>
+        <div className="px-10">
+          <img
+            src={scissors}
+            alt="logo"
+            className="h-40 hover:cursor-pointer"
+          />
+          <p className=" text-center text-gray-800 dark:text-gray-100 text-lg font-black ">
+            Tesoura
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
