a5cad2073756aac7ad53578e9b25ba2f98fc8ebf
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-13 03:11:34 -0300

    tailwind css in game button

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -31,7 +31,7 @@ export default function Game() {
         </button>
         <button
           onClick={() => setScoreState(0)}
-          className="select-none rounded-lg ml-4 bg-red-700 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase  text-white shadow-md shadow-gray-900/10 transition-all"
+          className="select-none rounded-lg ml-4 bg-red-700 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase  text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         >
           Zerar Pontuação
         </button>
