5bf6971e2961573d638c95490fc381c0ff15e37e
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-13 20:13:46 -0300

    implementing location.state is undefined

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ import Play from "./components/Play";
 
 function App() {
   const location = useLocation();
-  const { scoreState } = location.state;
+  let { scoreState } = location.state ?? { scoreState: 0 };
 
   return (
     <>
       <div className="bg-red-400 h-screen">
         <div className="">
-          <Header score={scoreState} />
+          <Header score={scoreState!} />
         </div>
         <p className=" text-center text-gray-800 dark:text-gray-100 text-3xl font-bold mt-8">
           Escolha uma das opções:
         </p>
         <div className="flex justify-center">
-          <Play score={scoreState} />
+          <Play score={scoreState!} />
         </div>
         <div className="fixed left-0 bottom-0 w-full mb-8 text-center">
           <Footer />
