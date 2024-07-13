aa0db8d811bde2e9a0cdf7efca0a4a8657d94575
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-13 01:05:05 -0300

    Implementing tailwind css in error page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,15 +1,28 @@
-import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function () {
-  const error: any = useRouteError();
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error?.statusText || error?.message}</i>
-      </p>
-    </div>
+    <section className="flex items-center h-screen w-screen p-16 bg-red-400 text-gray-800">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl text-gray-100">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl">
+            Desculpa, não conseguimos achar essa página.
+          </p>
+          <p className="mt-4 mb-8 text-gray-600">
+            Mas não se preocupe, pode encontrar muitas outras coisas na nossa
+            página inicial
+          </p>
+          <Link
+            className="px-8 py-3 font-semibold rounded bg-gray-900 text-gray-50"
+            to={"/"}
+          >
+            Voltar para o inicio
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
